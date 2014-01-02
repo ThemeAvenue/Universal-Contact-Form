6c4cbdb576b9ae1995a2fc71ffcac49d0def50c2
Julien Liabeuf <julien@liabeuf.fr>
2014-01-02 14:16:08 +0700

    Remove useless shortcode

--- public/includes/shortcodes.php
+++ public/includes/shortcodes.php
@@ -1,29 +1,4 @@
 <?php
-add_shortcode( 'ucf-input', 'ucf_input_field' );
-/**
- * Generate a form input
- */
-function ucf_input_field( $atts ) {
-
-	$shortcode = new Contact_Form_Generator();
-
-	extract( shortcode_atts( array(
-		'id' 		  => false,
-		'name' 		  => false,
-		'required' 	  => false,
-		'pattern' 	  => false,
-		'placeholder' => false,
-		'autofocus'   => false,
-		'title' 	  => false,
-		'min' 		  => false, 	// Date & number inputs only
-		'max' 		  => false, 	// Date & number inputs only
-		'multiple' 	  => false, 	// For input files only
-		'step' 		  => false,
-
-	), $atts ) );
-
-}
-
 add_shortcode( 'ucf-form', 'ucf_form' );
 /**
  * [ucf_form description]
